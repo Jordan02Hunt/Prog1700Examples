469832f1b9bdb54b49c5534b51fbb41b21795551
Jordan Hunt <jrhunt17@live.com>
2023-10-04 10:13:35 -0300

    Oct 3 Lecture Notes

---
+++ PowerShellBasics/Decisions/Roman.ps1
@@ -0,0 +1,40 @@
+[int]$number = Read-host "enter a number between 1 and 10"
+if ($number -eq 1){
+    Write-host "I"
+}
+
+if ($number -eq 2){
+    Write-host "II"
+}
+
+if ($number -eq 3){
+    Write-host "III"
+}
+
+if ($number -eq 4){
+    Write-host "IV"
+}
+
+if ($number -eq 5){
+    Write-host "V"
+}
+
+if ($number -eq 6){
+    Write-host "VI"
+}
+
+if ($number -eq 7){
+    Write-host "VII"
+}
+
+if ($number -eq 8){
+    Write-host "VIII"
+}
+
+if ($number -eq 9){
+    Write-host "IX"
+}
+
+if ($number -eq 10){
+    Write-host "X"
+}

94c0faefb3e838cf3aa6a91f2489fc8c382e0bb4
Jordan Hunt <jrhunt17@live.com>
2023-11-03 10:03:05 -0300

    Functions

---
+++ PowerShellBasics/Functions/HighLow.ps1
@@ -0,0 +1,27 @@
+Function Get-HighLowAnswer {
+    #generate a random number
+    Get-random -Minimum 1 -Maximum 10
+}
+function Start-HighLowGame{
+    do{
+        #Ask the user to make a guess
+        $guess = Read-Host "Guess a number Between 1-10"
+
+        #compare guess against answer
+
+        #If the guess is lower then the answer, output Higher
+        if($guess -lt $answer){
+            "HIGHER"
+        }
+        #If the guess is higher then the answer, output Lower
+        if($guess -gt $answer){
+            "LOWER"
+        }
+        #if guess is equal to the answer, output correct
+        if($guess -eq $answer){
+            "CORRECT"
+        }
+        }until ($guess -eq $answer)
+}
+$answer = Get-HighLowAnswer
+Start-HighLowGame
